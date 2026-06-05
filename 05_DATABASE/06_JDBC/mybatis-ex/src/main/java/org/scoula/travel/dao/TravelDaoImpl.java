66f64f23be2e2e8e9476c27b7e28dabefaa92d71
JungHan-Seok <jhstone034679@gmail.com>
2026-06-05 09:41:55 +0900

    06_JDBC

---
+++ 05_DATABASE/06_JDBC/mybatis-ex/src/main/java/org/scoula/travel/dao/TravelDaoImpl.java
@@ -0,0 +1,82 @@
+package org.scoula.travel.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.scoula.travel.database.MyBatisConfig;
+import org.scoula.travel.domain.TravelVO;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+
+/*
+*   SqlSession 메서드
+*   - selectOne(id) : 결과가 정확히 1건인 select
+*   - selectList(id) : 결과가 0건 이상인 (응답이 List형태로나옴)
+*   - insert(id, param) : return이 영향받은 행의 수
+*   - update(id, param) : return이 영향받은 행의 수
+*   - delete(id, param) : return이 영향받은 행의 수
+*   Transaction
+*   - commit() : 변경사항을 DB에 반영
+*   - rollback() : 변경사항 취소
+* */
+public class TravelDaoImpl implements TravelDao{
+
+    static final String NAMESPACE = "org.scoula.travel.dao.TravelDao.";
+
+    @Override
+    public int getTotalCount() {
+
+        return MyBatisConfig.getSqlSession()
+                .selectOne(NAMESPACE + "getTotalCount");
+    }
+
+    @Override
+    public List<TravelVO> getTravels(int start, int end) {
+
+        return List.of();
+    }
+
+    @Override
+    public List<String> getDistricts() {
+        return MyBatisConfig.getSqlSession()
+                .selectList(NAMESPACE + "getDistricts");
+    }
+
+    @Override
+    public List<TravelVO> getTravelsByDistrict(String district) {
+        return MyBatisConfig.getSqlSession()
+                .selectList(NAMESPACE + "getTravelsByDistrict", district);
+    }
+
+    @Override
+    public Optional<TravelVO> getTravel(Long no) {
+        return Optional.empty();
+    }
+
+    /*
+    * 트랜젝션 처리 메서드
+    * */
+    private void transaction(Consumer<SqlSession> action) {
+        try (SqlSession session = MyBatisConfig.getSqlSession()) {
+            action.accept(session);
+            session.commit(); // db에 반영
+        }
+    }
+
+    @Override
+    public void insert(TravelVO travel) {
+
+        transaction(session -> session.insert(NAMESPACE + "insert", travel));
+    }
+
+    @Override
+    public void update(TravelVO travel) {
+        transaction(session -> session.update(NAMESPACE + "update", travel));
+    }
+
+    @Override
+    public void remove(Long no) {
+        transaction(session -> session.delete(NAMESPACE + "remove", no));
+    }
+}
