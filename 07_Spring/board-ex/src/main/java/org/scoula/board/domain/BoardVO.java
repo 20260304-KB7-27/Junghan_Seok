760b63e540b900afc71d33f056a525d238303743
JungHan-Seok <jhstone034679@gmail.com>
2026-06-23 09:28:07 +0900

    07_Spring board-ex

---
+++ 07_Spring/board-ex/src/main/java/org/scoula/board/domain/BoardVO.java
@@ -0,0 +1,20 @@
+package org.scoula.board.domain;
+
+import lombok.*;
+
+import java.util.Date;
+
+// Mybatis진영에서의 VO -> DB 테이블 한 행에 매핑되는 객체
+@Data // GETTER, SETTER, ToSTring, EqualsAndHashCode, RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BoardVO {
+
+    private Long no;
+    private String title;
+    private String content;
+    private String writer;
+    private Date regDate;
+    private Date updateDate;
+}
